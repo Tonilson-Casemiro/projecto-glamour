59e1636324421a2d52c5d1c29e7538aa0ae6441a
Tonilson António Casemiro <94199921+Tonilson-Casemiro@users.noreply.github.com>
2022-06-09 17:00:21 +0100

    mexendo no mobile

---
+++ script.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal({reset: true})
+
+sr.reveal('.logotipo',{
+    rotate: {x:0, y:5, z:0},
+    duration: 1000
+})
+
+sr.reveal('.conteudos-topo',{
+    rotate: {x:0, y:80, z:0},
+    duration: 2500
+})
