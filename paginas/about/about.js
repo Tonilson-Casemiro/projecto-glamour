472867ed20812b60029005c60839b460aa186cdf
Tonilson-Casemiro <casmirotonilson1@gmail.com>
2022-06-13 18:22:46 +0100

    Actualização de conteudo, adição do footer, e trabalhando na responsive

---
+++ paginas/about/about.js
@@ -0,0 +1,13 @@
+
+window.sr = ScrollReveal({reset: true})
+
+sr.reveal('.imagem-about',{
+  rotate: {x:0, y:20, z:0},
+  duration: 2500
+})
+
+sr.reveal('.conteudo-about',{
+  rotate: {x:0, y:20, z:0},
+  duration: 2500
+})
+
--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Configurando o menu mobile
+var iconeOpenMenu = document.getElementById('icone-open-menu')
+var iconeCloseMenu = document.getElementById('icone-close-menu')
+var nav = document.querySelector('nav')
+
+iconeOpenMenu.addEventListener('click', function(){
+    nav.classList.add('show')
+})
+
+iconeCloseMenu.addEventListener('click', function(){
+    nav.classList.remove('show')
+})
+
 window.sr = ScrollReveal({reset: true})
 
 sr.reveal('.logotipo',{
@@ -9,3 +22,9 @@ sr.reveal('.conteudos-topo',{
     rotate: {x:0, y:80, z:0},
     duration: 2500
 })
+
+sr.reveal('.cubo-servicos',{
+    rotate: {x:0, y:20, z:0},
+    duration: 2500
+})
+
