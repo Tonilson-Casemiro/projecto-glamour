a534a18d6e53da526c26ad700f2b8c3c5206b70f
Tonilson-Casemiro <casmirotonilson1@gmail.com>
2022-06-21 23:22:15 +0100

    scroll reveal nos contactos

--- paginas/contactos/contactos.js
+++ paginas/contactos/contactos.js
@@ -7,10 +7,36 @@ sr.reveal('.conteudo-centro-header',{
   duration: 2500
 })
 
-
-
-sr.reveal('.seccao-mapa',{
+sr.reveal('.container-contactos',{
   rotate: {x:0, y:10, z:0},
   duration: 2500
 })
+
+sr.reveal('.icones-seccao1',{
+  rotate: {x:0, y:10, z:0},
+  duration: 2500
+})
+
+
+sr.reveal('.conteudo-icone-seccao1',{
+  rotate: {x:0, y:10, z:0},
+  duration: 3100
+})
+
+sr.reveal('.conteudo-icone-seccao1',{
+  rotate: {x:0, y:10, z:0},
+  duration: 3100
+})
+
+
+sr.reveal('.seccao-formulario-email',{
+  rotate: {x:0, y:10, z:0},
+  duration: 3100
+})
+
+sr.reveal('.seccao1',{
+  rotate: {x:0, y:10, z:0},
+  duration: 3100
+})
+
 
