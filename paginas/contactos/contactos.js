8066ec27ae6bde532c29fae4a3ec319353cecc8f
Tonilson-Casemiro <casmirotonilson1@gmail.com>
2022-06-21 22:41:29 +0100

    adicionando o mapa e o pagina dos conatctos

---
+++ paginas/contactos/contactos.js
@@ -0,0 +1,16 @@
+/* Scroll reveal */
+
+window.sr = ScrollReveal({reset: true})
+
+sr.reveal('.conteudo-centro-header',{
+  rotate: {x:0, y:10, z:0},
+  duration: 2500
+})
+
+
+
+sr.reveal('.seccao-mapa',{
+  rotate: {x:0, y:10, z:0},
+  duration: 2500
+})
+
